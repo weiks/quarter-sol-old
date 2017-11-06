30338d172efc5b5d088c5c7ae148a34099dff930
Jaynti Kanani <jdkanani@gmail.com>
2017-11-07 00:23:12 +0530

    params

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var Quarters = artifacts.require("./Quarters.sol");
+var Quarters = artifacts.require('./Quarters.sol')
 
 module.exports = function(deployer) {
-  deployer.deploy(Quarters);
-};
+  deployer.deploy(Quarters, '1000000000', 'Quarters', 'QRT', '1000', '900000')
+}
