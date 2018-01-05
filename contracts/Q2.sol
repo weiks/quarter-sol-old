a1f7bf9a1a436383aedc279c189cbc913f4017c0
Jaynti Kanani <jdkanani@gmail.com>
2018-01-05 21:22:19 +0530

    add q2 and crowdsale

--- contracts/Q2.sol
+++ contracts/Q2.sol
@@ -3,33 +3,86 @@ pragma solidity ^0.4.18;
 import './Ownable.sol';
 import './StandardToken.sol';
 
-interface TokenRecipient {
-  function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public;
-}
-
 contract Q2 is Ownable, StandardToken {
   string public name = "Q2";
   string public symbol = "Q2";
   uint8 public decimals = 18;
 
-  /**
-   * Set allowance for other address and notify
-   *
-   * Allows `_spender` to spend no more than `_value` tokens in your behalf, and then ping the contract about it
-   *
-   * @param _spender The address authorized to spend
-   * @param _value the max amount they can spend
-   * @param _extraData some extra information to send to the approved contract
-   */
-  function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-  public
-  returns (bool success) {
-    TokenRecipient spender = TokenRecipient(_spender);
-    if (approve(_spender, _value)) {
-      spender.receiveApproval(msg.sender, _value, this, _extraData);
-      return true;
-    }
+  // token creation cap
+  uint256 public creationCap = 15000000 * (10**18); // 15M
+  uint256 public reservedFund = 10000000 * (10**18); // 10M
 
-    return false;
+  // events
+  event MintTokens(address indexed _to, uint256 _value);
+  event StageStarted(uint8 _stage, uint256 _totalSupply, uint256 _balance);
+  event StageEnded(uint8 _stage, uint256 _totalSupply, uint256 _balance);
+
+  // eth wallet
+  address public ethWallet;
+
+  // current state info
+  bool public running;
+  uint8 public stage;
+  uint256 public exchangeRate;
+  uint256 public startBlock;
+  uint256 public endBlock;
+
+  function Q2(address _ethWallet) public {
+    ethWallet = _ethWallet;
+
+    // reserved tokens
+    mintTokens(ethWallet, reservedFund);
+  }
+
+  function mintTokens(address to, uint256 value) internal {
+    require(value > 0);
+    balances[to] += value;
+    totalSupply += value;
+
+    require(totalSupply <= creationCap);
+    assert(totalSupply >= value);
+
+    MintTokens(to, value);
+  }
+
+  function () public payable {
+    buyTokens();
+  }
+
+  function buyTokens() public payable {
+    require(running);
+    require(msg.value > 0);
+    require(block.number >= startBlock && block.number < endBlock);
+
+    uint256 tokens = msg.value * exchangeRate;
+    mintTokens(msg.sender, tokens);
+  }
+
+  function startStage(uint256 _exchangeRate, uint256 _startBlock, uint256 _endBlock) public onlyOwner {
+    require(!running);
+    require(_exchangeRate > 0);
+    require(_startBlock > block.number);
+    require(_startBlock < _endBlock);
+
+    startBlock = _startBlock;
+    endBlock = _endBlock;
+    exchangeRate = _exchangeRate;
+    stage += 1;
+    running = true;
+
+    StageStarted(stage, totalSupply, this.balance);
+  }
+
+  function endStage() public onlyOwner {
+    require(running);
+    require(block.number > endBlock);
+
+    running = false;
+
+    StageEnded(stage, totalSupply, this.balance);
+  }
+
+  function withdraw() public onlyOwner {
+    ethWallet.transfer(this.balance);
   }
 }
